4d3be7c4d50f09199cd4ce95fda6a55ed654fa3f
atusi <atu.cho.33@gmail.com>
2022-10-25 14:44:21 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as faceapi from "face-api.js";
+import { useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "user",
+};
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+    ])
+      .then(console.log("done"))
+      .catch((e) => console.log(e));
+  }, []);
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(webcamRef, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    console.log(detections);
+  }, 1000);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam
+        audio={false}
+        width={540}
+        height={360}
+        ref={webcamRef}
+        videoConstraints={videoConstraints}
+      />
     </div>
   );
 }
